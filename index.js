13f159f46b134ab40c2b7f12faa93a47d2f35f2e
Henrique Buzin <cultura.henrique@gmail.com>
2024-04-07 09:14:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Dae man!\n');
+  res.end('So de boas?\n');
 });
 
 server.listen(port, hostname, () => {
