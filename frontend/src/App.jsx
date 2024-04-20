177a3eb7258417e4d81dd7ede8aa9789f539047f
Henrique Buzin <cultura.henrique@gmail.com>
2024-04-20 15:39:22 -0300

    TITLE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
+import React, { useEffect } from 'react';
 import Background from './components/Background/Background.jsx';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "Buzin Solutions";
+  }, []);
+
   return (
     <div className="App">
       <Background />
